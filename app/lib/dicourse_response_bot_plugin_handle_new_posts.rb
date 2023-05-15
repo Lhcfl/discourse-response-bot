dce64e30fc9a2ef3136e3d67f11c235871a72326
Lhcfl <Lhcfl@outlook.com>
2023-05-15 19:17:49 +0800

    satisfy pritter

--- app/lib/dicourse_response_bot_plugin_handle_new_posts.rb
+++ app/lib/dicourse_response_bot_plugin_handle_new_posts.rb
@@ -2,7 +2,6 @@
 
 DiscourseEvent.on(:post_created) do |*params|
   if SiteSetting.dicourse_response_bot_plugin_enabled
-
     # Get params of post you created.
     post, opt, user = params
 
@@ -13,33 +12,37 @@ DiscourseEvent.on(:post_created) do |*params|
       {
         # Handle post like `@bot cmd1 cmd2 cmd3`
         "@#{bot.username&.downcase}" => ->(cmds) do
-
           guardian = Guardian.new(user)
 
           # varibles
           @values = {}
 
           def calc(data = "")
-            data&.gsub(/{[\s\S]+?\}/) do |b|
-              @values[b[1..-2]]
-            end
+            data&.gsub(/{[\s\S]+?\}/) { |b| @values[b[1..-2]] }
           end
-          
-          cmdList = cmds.split(' ')
 
-          settingList = SiteSetting.dicourse_response_bot_plugin_bot_command_variables?.split('|')
-          
+          cmdList = cmds.split(" ")
+
+          settingList = SiteSetting.dicourse_response_bot_plugin_bot_command_variables?.split("|")
+
           # combine cmd and settings
           [cmdList.size, settingList.size].min.times do |i|
-            str, exprs = cmdList[i], settingList[i].split(',')
-            @values['%s'] = str
+            str, exprs = cmdList[i], settingList[i].split(",")
+            @values["%s"] = str
 
             exprs.each do |expr|
               # Ternary operator
-              matched = expr.match /([a-zA-Z]+[0-9a-zA-Z]*)=([\s\S]*?)==([\s\S]*?)\?([\s\S]*?):([\s\S]*)/
+              matched =
+                expr.match /([a-zA-Z]+[0-9a-zA-Z]*)=([\s\S]*?)==([\s\S]*?)\?([\s\S]*?):([\s\S]*)/
               if matched
-                @values[matched[1]] = calc(matched[2]) == calc(matched[3]) ? calc(matched[4]) : calc(matched[5])
-              else 
+                @values[matched[1]] = (
+                  if calc(matched[2]) == calc(matched[3])
+                    calc(matched[4])
+                  else
+                    calc(matched[5])
+                  end
+                )
+              else
                 matched = expr.match /([a-zA-Z]+[0-9a-zA-Z]*)=([\s\S]*)/
                 @values[matched[1]] = calc(matched[2])
               end
@@ -53,11 +56,9 @@ DiscourseEvent.on(:post_created) do |*params|
             # import_mode: true,
             skip_validations: true,
           )
-
         end,
       },
-    # BOT SHOULD NOT HANDLE ITSELF
+      # BOT SHOULD NOT HANDLE ITSELF
     ) if user.id != bot.id
   end
 end
-
