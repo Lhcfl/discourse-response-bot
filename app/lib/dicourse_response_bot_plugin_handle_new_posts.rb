01d1a29fa89b20310259c22c49be89c808c5adda
Lhcfl <Lhcfl@outlook.com>
2023-05-16 01:05:06 +0800

    response to any context

--- app/lib/dicourse_response_bot_plugin_handle_new_posts.rb
+++ app/lib/dicourse_response_bot_plugin_handle_new_posts.rb
@@ -7,58 +7,55 @@ DiscourseEvent.on(:post_created) do |*params|
 
     bot = DiscourseResponseBot.getBot()
 
-    DiscourseResponseBot.botParseCmd(
-      post.raw,
-      {
-        # Handle post like `@bot cmd1 cmd2 cmd3`
-        "@#{bot.username&.downcase}" => ->(cmds) do
-          guardian = Guardian.new(user)
+    if user.id != bot.id && post.raw
+      to_response = "@#{bot.username&.downcase}"
 
-          # varibles
-          @values = {}
+      slice_p = post.raw.downcase.index to_response
 
-          def calc(data = "")
-            data&.gsub(/{[\s\S]+?\}/) { |b| @values[b[1..-2]] }
-          end
+      cmds = post.raw[slice_p + to_response.length + 1..]
 
-          cmdList = cmds.split(" ")
+      # varibles
+      @values = {}
 
-          settingList = SiteSetting.dicourse_response_bot_plugin_bot_command_variables?.split("|")
+      def calc(data = "")
+        data&.gsub(/{[\s\S]+?\}/) { |b| @values[b[1..-2]] }
+      end
 
-          # combine cmd and settings
-          [cmdList.size, settingList.size].min.times do |i|
-            str, exprs = cmdList[i], settingList[i].split(",")
-            @values["%s"] = str
+      cmdList = cmds.split(" ")
 
-            exprs.each do |expr|
-              # Ternary operator
-              matched =
-                expr.match /([a-zA-Z]+[0-9a-zA-Z]*)=([\s\S]*?)==([\s\S]*?)\?([\s\S]*?):([\s\S]*)/
-              if matched
-                @values[matched[1]] = (
-                  if calc(matched[2]) == calc(matched[3])
-                    calc(matched[4])
-                  else
-                    calc(matched[5])
-                  end
-                )
+      settingList = SiteSetting.dicourse_response_bot_plugin_bot_command_variables?.split("|")
+
+      # combine cmd and settings
+      [cmdList.size, settingList.size].min.times do |i|
+        str, exprs = cmdList[i], settingList[i].split(",")
+        @values["%s"] = str
+
+        exprs.each do |expr|
+          # Ternary operator
+          matched =
+            expr.match /([a-zA-Z]+[0-9a-zA-Z]*)=([\s\S]*?)==([\s\S]*?)\?([\s\S]*?):([\s\S]*)/
+          if matched
+            @values[matched[1]] = (
+              if calc(matched[2]) == calc(matched[3])
+                calc(matched[4])
               else
-                matched = expr.match /([a-zA-Z]+[0-9a-zA-Z]*)=([\s\S]*)/
-                @values[matched[1]] = calc(matched[2])
+                calc(matched[5])
               end
-            end
+            )
+          else
+            matched = expr.match /([a-zA-Z]+[0-9a-zA-Z]*)=([\s\S]*)/
+            @values[matched[1]] = calc(matched[2])
           end
+        end
+      end
 
-          DiscourseResponseBot.botSendPost(
-            post.topic_id,
-            calc(SiteSetting.dicourse_response_bot_plugin_bot_response?),
-            reply_to_post_number: post.post_number,
-            # import_mode: true,
-            skip_validations: true,
-          )
-        end,
-      },
-      # BOT SHOULD NOT HANDLE ITSELF
-    ) if user.id != bot.id
+      DiscourseResponseBot.botSendPost(
+        post.topic_id,
+        calc(SiteSetting.dicourse_response_bot_plugin_bot_response?),
+        reply_to_post_number: post.post_number,
+        # import_mode: true,
+        skip_validations: true,
+      )
+    end
   end
 end
